9dcfa53bcd73ce7a853f5443ef367643b9721d4b
Guillaume Anthouard <guillaume.anthouard@hotmail.fr>
2021-07-17 22:33:14 +0200

    In progress fix for fields that return Map

--- src/marais/graphql/dsl/fetchers.kt
+++ src/marais/graphql/dsl/fetchers.kt
@@ -6,12 +6,10 @@ import graphql.schema.DataFetchingEnvironment
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.future.asCompletableFuture
 import kotlinx.coroutines.future.future
 import kotlinx.coroutines.reactive.asPublisher
-import java.util.concurrent.CompletableFuture
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 import kotlin.reflect.KFunction
@@ -42,9 +40,12 @@ fun transformAsyncResult(value: Any?, context: CoroutineContext = EmptyCoroutine
 /**
  * @param receiver should not be null for root queries
  */
-fun <R, O> propertyFetcher(property: KProperty1<R, O>, receiver: R? = null): DataFetcher<Any?> {
-    return receiver?.let { TrivialDataFetcher { property.get(receiver) } }
-        ?: TrivialDataFetcher { property.get(it.getSource()) }
+fun <R, O> propertyFetcher(property: KProperty1<R, O>, receiver: R?): DataFetcher<Any?> {
+    return if (receiver == null) {
+        TrivialDataFetcher { transformMap(property.get(it.getSource())) }
+    } else {
+        TrivialDataFetcher { transformMap(property.get(receiver)) }
+    }
 }
 
 /**
@@ -60,11 +61,13 @@ fun functionFetcher(
 ): DataFetcher<Any?> {
     return if (func.isSuspend) {
         suspendFetcher(scope, context) { env ->
-            func.callSuspend(
-                receiver ?: env.getSource(),
-                *args.map {
-                    it.resolve<Any>(env)
-                }.toTypedArray()
+            transformMap(
+                func.callSuspend(
+                    receiver ?: env.getSource(),
+                    *args.map {
+                        it.resolve<Any>(env)
+                    }.toTypedArray()
+                )
             )
         }
     } else {
@@ -92,7 +95,7 @@ fun <O> suspendFetcher(
 ): DataFetcher<Any?> {
     return DataFetcher {
         scope.future(context) {
-            transformAsyncResult(inner(it), context)
+            transformMap(transformAsyncResult(inner(it), context))
         }
     }
 }
