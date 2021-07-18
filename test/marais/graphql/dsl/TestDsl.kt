e2ba86fcbd47da818ae7ef7f3e656cb95fa135a9
Guillaume Anthouard <guillaume.anthouard@hotmail.fr>
2021-07-18 15:11:29 +0200

    Add more tests

--- test/marais/graphql/dsl/TestDsl.kt
+++ test/marais/graphql/dsl/TestDsl.kt
@@ -1,142 +1,62 @@
 package marais.graphql.dsl
 
-import graphql.GraphQL
-import graphql.schema.DataFetchingEnvironment
+import graphql.language.StringValue
+import graphql.schema.*
 import org.slf4j.LoggerFactory
 import kotlin.test.Test
+import kotlin.test.assertContains
 import kotlin.test.assertEquals
-import kotlin.test.assertFailsWith
 import kotlin.test.assertTrue
 
 class TestDsl {
     private val log = LoggerFactory.getLogger(TestDsl::class.java)
 
-    // TODO make more smaller test
-
     @Test
-    fun testDsl() {
-        val startTime = System.currentTimeMillis()
-        val builder = SchemaBuilder {
-
-            scalar("Url", UrlCoercing)
-            id<MyId> {
-                it?.let { MyId(it) }
-            }
-
-            enum<Baz>()
-
-            input<Input>()
-
-            !"This describes my Node interface"
-            inter<Node> {
-                derive()
-
-                !"Description on a field too !"
-                field("parent") { ->
-                    MyId("Node:" + id.inner)
-                }
-            }
-
-            !"""
-                This is a cool looking multiline description
-                No need to call .trimIndent()
-            """
-            type<Foo> {
-                // TODO specifying interface on a type should automatically declare appropriate fields
-                inter<Node>()
-
-                // Can be a property
-                include(Foo::id)
-                // Can be a member function
-                include(Foo::dec)
-                assertFailsWith<Exception>("We already included that field with the same name") {
-                    include(Foo::dec)
-                }
-                include(Foo::field)
-                // Can be a custom function
-                field("inc") { _: DataFetchingEnvironment ->
-                    field + 1
-                }
-            }
-
-            type<Bar> {
-                inter<Node>()
-
-                derive {
-                    -Bar::field
-
-                    assertFailsWith<Exception>("We already excluded this field") {
-                        exclude(Bar::field)
-                    }
-                }
-
-                field("custom") { param: String ->
-                    param
-                }
-
-                field("custom2") { a: List<Int>, b: Int ->
-                    a.map { it * b }
-                }
-
-                field("custom3") { a: Int, b: Float, c: MyId ->
-                    "$c: ${a * b}"
-                }
-            }
-
-            query(Query)
+    fun testSimpleSchema() = withSchema({
+        query {
+            "answer" { -> 42 }
         }
-        val initTime = System.currentTimeMillis() - startTime
-        val schema = builder.build()
-        val buildTime = System.currentTimeMillis() - startTime
-        log.debug("Schema build time : $buildTime ms (init : $initTime ms)")
-
-        log.debug(schema.print(includeDirectives = false))
-
-        val graphql = GraphQL.newGraphQL(schema).build()
-        val result = graphql.execute(
-            """
-            query {
-              foo {
-                id,
-                field,
-                dec,
-                inc,
-                parent
-              },
-              bar {
-                id,
-                custom(param: "hello"),
-                custom2(a: [1, 2, 3], b: 4)
-              },
-              node {
-                id,
-                parent,
-                ... on Foo { value: field },
-                ... on Bar { custom(param: "hello") }
-              },
-              suspendFun,
-              deferedFun,
-              futureFun,
-              acceptId(id: "yay")
-            }
-        """.trimIndent()
-        )
-        log.debug(result.getData<Map<String, Any?>?>()?.toString())
-        assertTrue(result.errors.isEmpty(), "${result.errors}")
-        log.debug(result.extensions?.toString())
+    }) {
+        assertEquals(mapOf("answer" to 42), execute("""query { answer }""").getData())
     }
 
     @Test
-    fun testMap() {
+    fun testScalar() {
+
+        data class Handle(val inner: String)
+
         withSchema({
-            query(object {
-                val prop = mapOf("key" to "value")
+            scalar("Handle", object : Coercing<Handle, String> {
+                override fun serialize(dataFetcherResult: Any?): String {
+                    return when (dataFetcherResult) {
+                        is Handle -> dataFetcherResult.inner
+                        else -> throw CoercingSerializeException("Must be Handle")
+                    }
+                }
+
+                override fun parseValue(input: Any?): Handle {
+                    return when (input) {
+                        is String -> Handle(input)
+                        else -> throw CoercingParseValueException("Must be String")
+                    }
+                }
+
+                override fun parseLiteral(input: Any?): Handle {
+                    return when (input) {
+                        is StringValue -> Handle(input.value)
+                        else -> throw CoercingParseLiteralException("Must be String")
+                    }
+                }
             })
+
+            query {
+                "test" { handle: Handle -> handle }
+            }
         }) { schema ->
-            log.debug(schema.print(includeDirectives = false))
+            assertContains(schema.print(includeDirectives = false), "scalar Handle")
             assertEquals(
-                mapOf("prop" to listOf(mapOf("key" to "key", "value" to "value"))),
-                execute("query { prop { key, value } }").getData()
+                mapOf("test" to "hello"),
+                execute("""query { test(handle: "hello") }""").getData()
             )
         }
     }
@@ -155,10 +75,56 @@ class TestDsl {
                     id.inner
                 }
             }
-        }) { schema ->
-            log.debug(schema.print(includeDirectives = false))
-
+        }) {
             assertEquals(mapOf("node" to "hello"), execute("""query { node(id: "hello") }""").getData())
         }
+    }
+
+    @Test
+    fun testEnum() = withSchema({
+        enum<Baz>()
+
+        query {
+            "test" { -> Baz.VALUE0 }
+        }
+    }) { schema ->
+        assertContains(schema.typeMap, "Baz")
+        assertEquals(3, (schema.typeMap["Baz"] as GraphQLEnumType).values.size)
+    }
+
+    @Test
+    fun testInput() {
+        data class MyInput(
+            val data: String,
+            val other: Int
+        )
+
+        withSchema({
+            input<MyInput>()
+
+            query {
+                "test" { input: MyInput -> "$input.data $input.other" }
+            }
+        }) {
+            val result = execute("""query { test(input: { data: "hello", other: 42 }) }""")
+            println(result.errors)
+            assertTrue(result.errors.isEmpty())
+            assertEquals(
+                mapOf("test" to "hello 42"),
+                result.getData()
+            )
+        }
+    }
+
+    @Test
+    fun testSimpleMap() = withSchema({
+        query(object {
+            val prop = mapOf("key" to "value")
+        })
+    }) {
+        assertEquals(
+            mapOf("prop" to listOf(mapOf("key" to "key", "value" to "value"))),
+            execute("query { prop { key, value } }").getData()
+        )
     }
 }
--- test/marais/graphql/dsl/helpers.kt
+++ test/marais/graphql/dsl/helpers.kt
@@ -2,8 +2,12 @@ package marais.graphql.dsl
 
 import graphql.GraphQL
 import graphql.schema.GraphQLSchema
+import org.slf4j.LoggerFactory
+
+private val log = LoggerFactory.getLogger("Schema")
 
 fun withSchema(schemaBuilder: SchemaSpec.() -> Unit, block: GraphQL.(schema: GraphQLSchema) -> Unit) {
     val schema = SchemaBuilder(schemaBuilder).build()
+    log.debug(schema.print(includeDirectives = false))
     block(GraphQL.newGraphQL(schema).build(), schema)
 }
